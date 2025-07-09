3be786d0afb20a35a6f0862fb64154d48444c4ff
MRdjenssen <sam123janssen@gmail.com>
2025-07-09 11:56:58 +0200

    debug1

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
 import './index.css';
-import { StrictMode, useState } from 'react';
+import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import TabletView from './TabletView.jsx';
-import AdminLogin from './AdminLogin.jsx';
 import AdminPanel from './AdminPanel.jsx';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
   const isAdmin = window.location.pathname === '/admin';
-
-  if (isAdmin && !loggedIn) {
-    return <AdminLogin onLogin={() => setLoggedIn(true)} />;
-  }
-
-  if (isAdmin && loggedIn) {
-    return <AdminPanel />;
-  }
-
-  return <TabletView />;
+  return isAdmin ? <AdminPanel /> : <TabletView />;
 }
 
 createRoot(document.getElementById('root')).render(
